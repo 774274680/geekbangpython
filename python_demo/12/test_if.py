7415242a12fb793df4cfa1ca7494f0480cb096d3
Learn more,do action <774274680@qq.com>
2019-12-12 15:43:55 +0800

    Update test_if.py
    学习补充

--- python_demo/12/test_if.py
+++ python_demo/12/test_if.py
@@ -3,3 +3,15 @@ if x == 'abc' :
     print('x 的值和abc 相等')
 else:
     print('x和 abc不相等')
+    
+#例子：
+age = int(input('请输入你的年龄'))
+
+if age >40:
+	print('年龄大于40岁')
+elif age>30:
+	print('年龄大于30岁小于40岁')
+elif age>20:
+	print('年龄大于20，小于30')
+else:
+	print('年龄太小')
